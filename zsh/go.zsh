9ec8bcb9bf6f1d2471212a5747f8f78a743da9ff
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2014-11-21 18:36:54 +0200

    fix: better go env handling

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,8 +1,12 @@
-# Add the GOPATH
-export GOPATH=/Users/kenneth/Code/go
+if [[ -d ~/Code/go ]]; then
+  # Add the GOPATH
+  export GOPATH=~/Code/go
+fi
 
-# Add the GOROOT, used by macvim
-export GOROOT=$(brew --prefix go)/libexec
+if (( $+commands[brew] )) ; then
+  # Add the GOROOT, used by macvim
+  export GOROOT=$(brew --prefix go)/libexec
 
-# Append the gopath bin to our own PATH
-export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+  # Append the gopath bin to our own PATH
+  export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+fi
