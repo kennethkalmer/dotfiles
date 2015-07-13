6543b30df044a8205b6475fb866eb0d4c6276701
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-07-13 15:45:10 +0200

    fix: don't assume nvm and/or homebrew on all boxes

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,2 +1,9 @@
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+# Only if nvm is here
+if [[ -d ~/.nvm ]] ; then
+  export NVM_DIR=~/.nvm
+
+  # Yank nvm out of homebrew
+  if (( $+commands[brew] )); then
+    source $(brew --prefix nvm)/nvm.sh
+  fi
+fi
