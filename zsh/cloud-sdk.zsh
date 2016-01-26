c31a7f74bdb48b75187d2212e089dde1b835c5a7
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2016-01-26 11:31:06 +0200

    chore: google cloud-sdk

---
+++ zsh/cloud-sdk.zsh
@@ -0,0 +1,4 @@
+if [[ -f '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]]; then
+  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+fi
