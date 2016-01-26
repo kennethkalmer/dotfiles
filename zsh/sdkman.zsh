4f36343c79fafa0e02450aadb628f86fb471e434
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2016-01-26 11:28:24 +0200

    chore: sdkman

--- zsh/sdkman.zsh
+++ zsh/sdkman.zsh
@@ -1,1 +1,2 @@
-[[ -f "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
