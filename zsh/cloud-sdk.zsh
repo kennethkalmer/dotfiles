d4c8e24dc272341bd89191cac7f3a4869ab02f56
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2018-05-02 08:36:14 +0200

    chore: remove cloud-sdk configs

--- zsh/cloud-sdk.zsh
+++
@@ -1,4 +0,0 @@
-if [[ -f '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]]; then
-  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
-fi
