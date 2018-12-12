e75cd51ea987a4018d12e5d59e1962b724008dc8
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2018-12-12 14:38:41 +0000

    chore: tracking changes

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,8 +1,7 @@
-{:repl {:plugins [[cider/cider-nrepl "0.17.0-SNAPSHOT"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]]
+{:repl {:plugins [[cider/cider-nrepl "0.18.0"]
+                  [refactor-nrepl "2.4.0"]]
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.13"]
-                       [proto-repl "0.3.1"]]}
+                       [org.clojure/tools.nrepl "0.2.13"]]}
  :user {:plugins [[lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]
                   [venantius/ultra "0.5.2"]
